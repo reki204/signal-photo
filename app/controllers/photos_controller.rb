025612008a45b14d13a88a2633655919e4d1a77f
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-11-29 18:22:09 +0900

    homeのcss修正

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,5 @@
 class PhotosController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :index
   def index
     # if params[:search] == nil
     #   @tweets= Photo.none
