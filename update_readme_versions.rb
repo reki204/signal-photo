18e3b655403a6f8fe87c9476fbe0a21f184b5dbb
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-01-19 14:09:03 +0900

    readmeの更新処理修正

--- update_readme_versions.rb
+++ update_readme_versions.rb
@@ -1,9 +1,8 @@
-# Gemfile からバージョンを抽出して README に追加するスクリプト
-
 require 'bundler'
 
 # Gemfile のパス
 gemfile_path = 'Gemfile'
+readme_path = 'README.md'
 
 # Gemfile から Ruby と Rails のバージョンを抽出
 ruby_version = nil
@@ -21,6 +20,21 @@ end
 if ruby_version && rails_version
   puts "Ruby Version: #{ruby_version}"
   puts "Rails Version: #{rails_version}"
+
+  readme_content = File.read(readme_path)
+
+  # Version セクションのバージョン情報
+  version_info = "Versions\n- Ruby: #{ruby_version}\n- Rails: #{rails_version}\n"
+
+  # バージョン情報がすでに存在する場合は上書き
+  if readme_content.include?("Versions")
+    readme_content.gsub!(/Versions\n- Ruby: .*\n- Rails: .*\n/, version_info)
+  else
+    # 存在しない場合はファイルの末尾に追加
+    readme_content += "\n#{version_info}"
+  end
+
+  File.write(readme_path, readme_content)
 else
   puts "必要なバージョン情報が Gemfile に見つかりませんでした。"
 end
