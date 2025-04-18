a21e88a0287706181edcd55ba3b4f8c1e417180e
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-04-18 21:51:11 +0900

    モデル修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -9,12 +9,11 @@ class Photo < ApplicationRecord
     validates :salt
   end
 
-  validates :password, :images, :encrypt_password, :salt, presence: true
-
   def self.password_matches?(password)
-    password.present? ? where(password: password) : none
+    password.present? ? where(password: password) : all
   end
 
+  # 暗号化パスワードとソルトを生成
   def generate_encrypt_password_and_salt
     self.encrypt_password = SecureRandom.hex(16)
     self.salt = SecureRandom.hex(8)
@@ -22,16 +21,18 @@ class Photo < ApplicationRecord
 
   # 画像を暗号化してJSONに保存する
   def encrypt_and_save_image_to_json(image_path, json_path)
-    encryptor = ImageEncryptor.new(image_path, encrypt_password, salt)
-    encryptor.process_and_save_image_to_json(json_path)
+    raise "Image file not found at path: #{image_path}" unless File.exist?(image_path)
+
+    encryptor = ImageEncryptor.new(encrypt_password, salt)
+    encryptor.process_and_save_image_to_json(image_path, json_path)
   end
 
-  # 復号化
-  def decrypt_and_decode_to_image(encrypted_data, encrypted_password, salt)
-    return unless File.exist?(encrypted_data)
+  # 復号化して画像バイナリを返す
+  def decrypt_and_decode_to_image(encrypted_json_path, encrypted_password, salt)
+    raise "Encrypted file not found at path: #{encrypted_json_path}" unless File.exist?(encrypted_json_path)
 
-    encrypted_json_data = JSON.parse(File.read(encrypted_data))
-    decryptor = ImageEncryptor.new(encrypted_json_data, encrypted_password, salt)
+    encrypted_json_data = JSON.parse(File.read(encrypted_json_path))
+    decryptor = ImageEncryptor.new(encrypted_password, salt)
     decryptor.decrypt_image(encrypted_json_data)
   end
 end
